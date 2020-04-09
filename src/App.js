6fbba546caeca4b911988718c9e719f7b313450f
max <maxwellrocco79@gmail.com>
2020-04-09 16:07:52 -0400

    I am so confused
    I need to talk to someone because none of this is working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component } from 'react';
+import './App.css';
+import Todo from './Todo';
+
+class App extends Component {
+  render() {
+    return (
+  <div id="01234" className="todo">
+          <button className="complete">Check</button>
+          <p>Do A thing</p>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default Todo;
---
+++ src/NewTodo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import '.NewTodo.css';
+import NewTodo from './NewTodo';
+class NewTodo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ToDo App</h1>
+      </div>
+render() {
+  return (
+    <div id="01234" className="todo">
+      <button className="complete">Check</button>
+      <p>Do A thing</p>
+    </div>
+  );
+}
+render(
+<div class="content">
+  <div id="todos">
+    <!-- form -->
+    <form id="todoInput">
+      <input id="newItem" type="text" alt= "input new item for todo list" placeholder="New Item..."/>
+      <!-- submit item button -->
+      <button id="submitItem">+</button>
+      </form>
+  </div>
+</div>
+
+    );
+  }
+}
+
+export default NewTodo;
